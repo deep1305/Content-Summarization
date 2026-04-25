3bcacdbc165b3b0e640298e9189c310fda3e8296
deep1305 <shahdeep1399@Gmail.com>
2026-04-25 19:14:52 -0400

    folder structure created

---
+++ template.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+
+project_name = "Content-Summarization"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+]
+
+base_dir = Path(__file__).resolve().parent
+
+for relative_path in list_of_files:
+    filepath = base_dir / relative_path
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    logging.info(f"Creating directory: {filepath.parent} for the file: {filepath.name}")
+
+    if (not filepath.exists()) or (filepath.stat().st_size == 0):
+        filepath.touch()
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"File already exists: {filepath}")
